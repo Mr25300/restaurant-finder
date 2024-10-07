2c760b9dbdc32a6f24bbdd210086dc974abc39e1
Ali-Akber Raza <aaoblivion@gmail.com>
2024-10-07 14:45:57 -0400

    added temp testing functionality

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
+import { storeSearch } from "./scripts/algorithms/store-search";
+//TEMP CODE FOR TESTING 
+import { readFileSync } from 'fs';
+// Load JSON text from server hosted file and return JSON parsed object
+function loadJSON(filePath: string): any {
+  // Load json file;
+  const json: string | null = readFileSync(filePath, 'utf8');
+
+  if (json) {
+    // Parse json
+    return JSON.parse(json);
+  }
+  return null;
+}
+
+
+
 //Don't remove this
-interface Data {
+export interface Data {
   ID: string[];
   storeName: string[];
   type: string[];
@@ -11,6 +28,4 @@ interface Data {
 
 const data: Data = loadJSON("../DO_NOT_TOUCH/data.json") as Data; //Don't delete this line. All your data is here. It does take a few seconds for Replit to load the data because it's so large.
 
-//This below just shows you how to access the data. You can remove these.
-console.log(data);
-console.log(data.ID);
+console.log(storeSearch(data, undefined, "Bistro / Rustic", undefined, undefined))
