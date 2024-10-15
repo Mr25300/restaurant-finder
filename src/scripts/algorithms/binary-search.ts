a2c54db3ef9c7d5b7e1a1395814582b49c459a55
Mr25300 <109829617+Mr25300@users.noreply.github.com>
2024-10-15 11:33:59 -0400

    intersections

--- src/scripts/algorithms/binary-search.ts
+++ src/scripts/algorithms/binary-search.ts
@@ -73,3 +73,39 @@ function filterStrings(data: string[], sortedIndices: Uint32Array, searchInput: 
     return 0;
   });
 }
+
+function getIntersections(...data: Uint32Array[]): number[] {
+  const existing: number[] = [];
+  const setCount = data.length;
+
+  for (let i = 0; i < setCount; i++) {
+    for (let j = 0; j < data[i].length; j++) {
+      const val = data[i][j];
+      const current = existing[val];
+
+      if (current == null) existing[val] = 0;
+      else existing[val]++;
+    }
+  }
+
+  const common: number[] = [];
+  let pointer = 0;
+
+  existing.forEach((value: number, index: number) => {
+    if (value == setCount - 1) {
+      common[pointer] = index;
+      pointer++;
+    }
+  });
+
+  return common;
+}
+
+let t11 = performance.now();
+console.log(getIntersections(
+  new Uint32Array([0, 1, 2]),
+  new Uint32Array([1, 2, 3]),
+  new Uint32Array([2, 3, 4])
+));
+let t22 = performance.now();
+console.log("Above performance ^^^", t22 - t11);
