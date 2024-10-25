9e29e2e805475d2a9080a7d1194e440de90824bc
Mr25300 <109829617+Mr25300@users.noreply.github.com>
2024-10-25 18:27:11 -0400

    more map stuff

--- src/scripts/components/map.ts
+++ src/scripts/components/map.ts
@@ -66,19 +66,19 @@ class DisplayMap {
 
     this.chunkColumns = Math.ceil(xRange / DisplayMap.CHUNK_SIZE);
     this.chunkRows = Math.ceil(yRange / DisplayMap.CHUNK_SIZE);
-    this.chunkCount = this.chunkRows*this.chunkColumns;
+    this.chunkCount = this.chunkRows * this.chunkColumns;
 
     this.chunks = new Array(this.chunkCount);
 
     for (let x = 0; x < this.chunkColumns; x++) {
       for (let y = 0; y < this.chunkRows; y++) {
-        const chunkCount = x*this.chunkRows + y;
+        const chunkCount = x * this.chunkRows + y;
 
         const cY = chunkCount % this.chunkRows;
-        const cX = (chunkCount - cY)/this.chunkRows;
+        const cX = (chunkCount - cY) / this.chunkRows;
 
-        const chunkMinX = this.minX + cX*DisplayMap.CHUNK_SIZE;
-        const chunkMinY = this.minY + cY*DisplayMap.CHUNK_SIZE;
+        const chunkMinX = this.minX + cX * DisplayMap.CHUNK_SIZE;
+        const chunkMinY = this.minY + cY * DisplayMap.CHUNK_SIZE;
         const chunkMaxX = chunkMinX + DisplayMap.CHUNK_SIZE - 1; // -1 so as to not include restaurants shared by the next chunk
         const chunkMaxY = chunkMinY + DisplayMap.CHUNK_SIZE - 1;
 
@@ -150,15 +150,15 @@ class DisplayMap {
 
     const cY = chunkNumber % this.chunkRows;
     const cX = (chunkNumber - cY) / this.chunkRows;
-    const chunkX = this.minX + cX*DisplayMap.CHUNK_SIZE + DisplayMap.CHUNK_SIZE/2 - this.cameraX;
-    const chunkY = this.minY + cY*DisplayMap.CHUNK_SIZE + DisplayMap.CHUNK_SIZE/2 - this.cameraY;
+    const chunkX = this.minX + cX * DisplayMap.CHUNK_SIZE + DisplayMap.CHUNK_SIZE / 2 - this.cameraX;
+    const chunkY = this.minY + cY * DisplayMap.CHUNK_SIZE + DisplayMap.CHUNK_SIZE / 2 - this.cameraY;
 
-    const screenX = width / 2 + chunkX / this.range * width / 2;
-    const screenY = height / 2 - chunkY / this.range * height / 2;
-    const size = width / (this.range * 2) * DisplayMap.CHUNK_SIZE;
+    const screenX = width/2 + chunkX/this.range*height/2;
+    const screenY = height/2 - chunkY/this.range*height/2;
+    const size = height/(this.range*2)*DisplayMap.CHUNK_SIZE;
 
-    context.fillStyle = `rgb(${(chunkNumber/3 % 1) * 255/2 + 255/2}, ${(chunkNumber/5 % 1) * 255/2 + 255/2}, ${(chunkNumber/4 % 1) * 255/2 + 255/2})`
-    context.fillRect(screenX - size/2, screenY - size/2, size, size);
+    context.fillStyle = `rgb(${(chunkNumber / 3 % 1) * 255 / 2 + 255 / 2}, ${(chunkNumber / 5 % 1) * 255 / 2 + 255 / 2}, ${(chunkNumber / 4 % 1) * 255 / 2 + 255 / 2})`
+    context.fillRect(screenX - size / 2, screenY - size / 2, size, size);
 
     context.fillStyle = "black";
 
@@ -167,43 +167,48 @@ class DisplayMap {
       const xPos = this.app.data.x[index] - this.cameraX;
       const yPos = this.app.data.y[index] - this.cameraY;
 
-      const screenX = width / 2 + xPos / this.range * width / 2;
-      const screenY = height / 2 - yPos / this.range * height / 2;
+      const screenX = width/2 + xPos/this.range*height/2;
+      const screenY = height/2 - yPos/this.range*height/2;
 
-      const size = width / this.range * 0.5;
+      const size = height/this.range*0.5;
 
       context.fillRect(screenX - size / 2, screenY - size / 2, size, size);
     }
   }
 
   updateDisplay() {
-    const t = performance.now();
     const width = CANVAS.width;
     const height = CANVAS.height;
-    const aspectRatio = width / height;
+    const aspectRatio = width/height;
     const context = CANVAS.getContext("2d") as CanvasRenderingContext2D;
 
     context.clearRect(0, 0, width, height);
 
     context.fillStyle = "red";
-    context.fillRect(width / 2 - width / this.range * 0.5 / 2, height / 2 - width / this.range * 0.5, width / this.range * 0.5, width / this.range * 0.5);
+    context.fillRect(width/2 - height/this.range*0.5/2, height/2 - height/this.range*0.5, height/this.range*0.5, height/this.range*0.5);
 
     context.fillStyle = "black";
 
-    const camChunkX = floor((this.cameraX - this.minX)/DisplayMap.CHUNK_SIZE);
-    const camChunkY = floor((this.cameraY - this.minY)/DisplayMap.CHUNK_SIZE);
+    const camChunkX = floor((this.cameraX - this.minX) / DisplayMap.CHUNK_SIZE);
+    const camChunkY = floor((this.cameraY - this.minY) / DisplayMap.CHUNK_SIZE);
 
     for (let x = 0; x <= 0; x++) {
       for (let y = 0; y <= 0; y++) {
         const xPos = camChunkX + x;
         const yPos = camChunkY + y;
-        const chunkNumber = xPos*this.chunkRows + yPos;
+        const chunkNumber = xPos * this.chunkRows + yPos;
 
         if (chunkNumber >= 0 && chunkNumber < this.chunkCount) this.displayChunk(chunkNumber, context, width, height);
       }
     }
 
-    const t2 = performance.now();
+    const chunkRangeX = Math.ceil(this.range*aspectRatio/DisplayMap.CHUNK_SIZE);
+    const chunkRangeY = Math.ceil(this.range*aspectRatio/DisplayMap.CHUNK_SIZE);
+    const xOffset = this.cameraX - camChunkX*DisplayMap.CHUNK_SIZE;
+    
+    console.log(xOffset);
+
+    console.log(chunkRangeX, chunkRangeY);
 
     // for (let i = 0; i < this.loaded.length; i++) {
     //   const index = this.loaded[i];
@@ -236,29 +241,29 @@ class DisplayMap {
     const rangeFloor = Math.floor(this.range);
     const rangeCeil = Math.ceil(this.range);
 
-    context.lineWidth = width/this.range*0.01;
+    context.lineWidth = width / this.range * 0.01;
 
     for (let x = -rangeFloor; x <= rangeFloor; x++) {
-        if (x == 0 || x == -rangeFloor || x == rangeFloor) context.strokeStyle = "red";
-        else context.strokeStyle = "black";
+      if (x == 0 || x == -rangeFloor || x == rangeFloor) context.strokeStyle = "red";
+      else context.strokeStyle = "black";
 
-        const xPos = x - this.cameraX % 1;
-        const screenPos = width/2 + xPos/this.range*width/2;
+      const xPos = x - this.cameraX % 1;
+      const screenPos = width / 2 + xPos / this.range * width / 2;
 
-        context.beginPath();
-        context.moveTo(screenPos, 0);
-        context.lineTo(screenPos, height);
-        context.stroke();
+      context.beginPath();
+      context.moveTo(screenPos, 0);
+      context.lineTo(screenPos, height);
+      context.stroke();
     }
 
-    for (let y = 0; y < this.range*2; y++) {
-        const yPos = y - this.cameraY % 1;
-        const screenPos = height/2 - yPos/this.range*height/2;
+    for (let y = 0; y < this.range * 2; y++) {
+      const yPos = y - this.cameraY % 1;
+      const screenPos = height / 2 - yPos / this.range * height / 2;
 
-        context.beginPath();
-        context.moveTo(0, screenPos);
-        context.lineTo(width, screenPos);
-        context.stroke();
+      context.beginPath();
+      context.moveTo(0, screenPos);
+      context.lineTo(width, screenPos);
+      context.stroke();
     }
   }
 
@@ -285,13 +290,11 @@ class DisplayMap {
     });
 
     CANVAS.addEventListener("mousedown", (event: MouseEvent) => {
-      if (event.button == 2) {
-        mouseX = event.clientX;
-        mouseY = event.clientY;
-      }
+      mouseX = event.clientX;
+      mouseY = event.clientY;
     });
 
-    CANVAS.addEventListener("mousemove", (event: MouseEvent) => {
+    document.addEventListener("mousemove", (event: MouseEvent) => {
       if (mouseX && mouseY) {
         const diffX = event.clientX - mouseX;
         const diffY = event.clientY - mouseY;
@@ -299,15 +302,13 @@ class DisplayMap {
         mouseX = event.clientX;
         mouseY = event.clientY;
 
-        this.moveCamera(-diffX * 2 * this.range / CANVAS.width, diffY * 2 * this.range / CANVAS.height);
+        this.moveCamera(-diffX/CANVAS.width*this.range*2, diffY/CANVAS.height*this.range*2);
       }
     });
 
-    CANVAS.addEventListener("mouseup", (event: MouseEvent) => {
-      if (event.button == 2) {
-        mouseX = null;
-        mouseY = null;
-      }
+    document.addEventListener("mouseup", (event: MouseEvent) => {
+      mouseX = null;
+      mouseY = null;
     });
 
     CANVAS.addEventListener("wheel", (event: WheelEvent) => {
